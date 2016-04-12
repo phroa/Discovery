d42ac8c693fb8943f5bf0021e273e3400ec35e73
Jack Stratton <jack@phroa.net>
2016-04-11 17:17:04 -0700

    Initial commit

---
+++ src/main/java/net/phroa/sponge/discovery/Discovery.java
@@ -0,0 +1,30 @@
+/**
+ * Discovery
+ *
+ * Copyright (C) phroa <jack@phroa.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.phroa.sponge.discovery;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "net.phroa.sponge.discovery",
+        name = "Discovery",
+        version = "0.0.1-SNAPSHOT",
+        description = "Discover fast-travel targets dynamically",
+        url = "https://github.com/phroa/Discovery")
+public class Discovery {
+
+}
